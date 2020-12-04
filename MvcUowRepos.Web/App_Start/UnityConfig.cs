0c14851940e244fb8f5d75f74b7dbbfd78622961
bunty choudhary <bchoudharyl@enestit.com>
2020-12-04 13:22:50 +0530

    Add project files.

---
+++ MvcUowRepos.Web/App_Start/UnityConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Unity;
+using MvcUowRepos.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unity.Mvc4;
+
+namespace MvcUowRepos.Web.App_Start
+{
+    public class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IMvcUow, MvcUow>();
+            container.RegisterType<IMvcUow, MvcUow>();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
